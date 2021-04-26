05fe90f87ba77b9a0ded5387d5b3029601e88ad2
Danil <danilsmy.edu@gmail.com>
2021-04-27 01:57:57 +0300

    Add find of some Matrix

--- CourseWork/CourseWork/matrix.cpp
+++ CourseWork/CourseWork/matrix.cpp
@@ -20,3 +20,45 @@ double** matrix::multiplyMatrix(double** Matr1, int n1, int m1, double** Matr2, 
 		return Matr;
 	}
 }
+
+double** matrix::crIdentityMatrix(int n)
+{
+	double** Matr = new double* [n];
+	for (int i = 0; i < n; i++) {
+		Matr[i] = new double[n];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j) {
+				Matr[i][j] = 1;
+			}
+			else {
+				Matr[i][j] = 0;
+			}
+		}
+	}
+	return Matr;
+}
+
+double** matrix::calculateMatrixB(double** Matr, int n)
+{
+	double** MatrB = crIdentityMatrix(n);
+	for (int j = 0; j < n; j++) {
+		if (j == n - 2) {
+			MatrB[n - 2][n - 2] = 1 / (Matr[n-1][n - 2]);
+		}
+		else {
+			MatrB[n - 2][j] = -1 * (Matr[n-1][j] / Matr[n-1][n - 2]);
+		}
+	}
+	return MatrB;
+}
+
+double** matrix::calculateMatrixBReverse(double** Matr, int n)
+{
+	double** MatrReverse = crIdentityMatrix(n);
+	for (int i = 0; i < n; i++) {
+		MatrReverse[n-2][i] = Matr[n - 1][i];
+	}
+	return MatrReverse;
+}
